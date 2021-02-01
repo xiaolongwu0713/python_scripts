ad29f106549c674cc8576f69d5bbcc15c32562dd
long <xiaolongwu0713@gmail.com>
2021-02-01 18:53:37 +0000

    EEGnet: follow the test.py to debug

--- others_works/EEGNet_in_pytorch/EEGnet_in_pytorch.py
+++ others_works/EEGNet_in_pytorch/EEGnet_in_pytorch.py
@@ -1,12 +1,9 @@
-from src.data import DataBuildClassifier
-import shutil
+from others_works.EEGNet_in_pytorch.src.data import DataBuildClassifier
 import os
 import numpy as np
-from sklearn.model_selection import train_test_split
-from src.utils import single_auc_loging
-from src.utils import prepare_dirs,write_results_table, separte_last_block
-from src.model_torch import train_model_eegnet
-import shutil
+from others_works.EEGNet_in_pytorch.src.utils import single_auc_loging
+from others_works.EEGNet_in_pytorch.src.utils import prepare_dirs,write_results_table, separte_last_block
+from others_works.EEGNet_in_pytorch.src.model_torch import train_model_eegnet
 from sklearn.model_selection import StratifiedKFold
 
 experiment_res_dir = './res/' #Path to save results and training|testing statistics
